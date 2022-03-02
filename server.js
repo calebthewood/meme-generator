dde5db0d727084b5f21f9eb9d53df8266b558b17
calebthewood <calebwood.cs@gmail.com>
2022-03-01 17:21:46 -0800

    adds server for deployment

---
+++ server.js
@@ -0,0 +1,21 @@
+const https = require('https');
+const express = require('express');
+const path = require('path');
+var helmet = require('helmet');
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+
+app.use(express.static(path.join(__dirname, '/')));
+// default URL for website
+
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to project folder.
+  });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000.");
+  });
